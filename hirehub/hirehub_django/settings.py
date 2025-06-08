80c02cec031cf1132f436c790f52e176004de000
amirhossein <amirhossein@gamertag.app>
2025-06-08 17:36:57 +0330

    [ADD] load environment variables, update applicant admin and requirements.txt

--- hirehub/hirehub_django/settings.py
+++ hirehub/hirehub_django/settings.py
@@ -12,10 +12,13 @@ https://docs.djangoproject.com/en/5.2/ref/settings/
 import os
 import dj_database_url
 from pathlib import Path
+from dotenv import load_dotenv 
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
+# Load environment variables from .env file
+load_dotenv(os.path.join(BASE_DIR, '.env'))
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/5.2/howto/deployment/checklist/
