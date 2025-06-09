8bfb51c2e059944186d49df8f176e80ad6c993ad
amirhossein <amirhossein@gamertag.app>
2025-06-09 15:19:21 +0330

    [REMOVE] temporary urls for error pages

--- hirehub/ats/urls.py
+++ hirehub/ats/urls.py
@@ -1,6 +1,5 @@
 from django.urls import path
 from . import views
-from django.views import defaults as default_views
 
 app_name = 'ats'
 
@@ -13,10 +12,4 @@ urlpatterns = [
     # API Endpoints
     path('api/applicants/', views.ApplicantListCreateAPIView.as_view(), name='api_applicant_list'),
     path('api/applicants/<int:pk>/', views.ApplicantDetailAPIView.as_view(), name='api_applicant_detail'),
-
-    # Temporary test paths for error pages
-    path('test-400/', default_views.bad_request, kwargs={'exception': Exception('Bad Request!')}, name='test_400'),
-    path('test-403/', default_views.permission_denied, kwargs={'exception': Exception('Permission Denied!')}, name='test_403'),
-    path('test-404/', default_views.page_not_found, kwargs={'exception': Exception('Page not Found!')}, name='test_404'),
-    path('test-500/', default_views.server_error, name='test_500'),
 ]
--- hirehub/hirehub_django/urls.py
+++ hirehub/hirehub_django/urls.py
@@ -14,15 +14,20 @@ Including another URLconf
     1. Import the include() function: from django.urls import include, path
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
+
 from django.contrib import admin
 from django.urls import path, include
 from django.conf import settings
+from django.conf.urls.static import static
+
 # from ats import views as ats_views # No longer needed for handlers if using string paths
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('ats/', include('ats.urls')),  # Include URLs from the ATS application
 ]
+if settings.DEBUG:
+    urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
 
 handler400 = 'ats.views.custom_bad_request'
 handler403 = 'ats.views.custom_permission_denied'
